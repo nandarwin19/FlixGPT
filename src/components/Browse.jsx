df2c33a8aaf7f2ebb0634b59c130c40cd5739ca1
nwin19 <nandarwinbuma@gmail.com>
2023-12-14 16:01:18 +0630

    Part 13: api data added into "moveSlice.js"

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/movieSlice.js";
 
 //We got 2 api. That is because of React.StrictMode. But for only local ( while developing )
 const Browse = () => {
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
@@ -12,6 +15,7 @@ const Browse = () => {
 
     const json = await data.json();
     console.log(json.results);
+    dispatch(addNowPlayingMovies(json.results));
   };
 
   // to run only one time
