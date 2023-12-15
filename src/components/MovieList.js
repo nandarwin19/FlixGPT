1913a9ea87b0ac8d056d9f386ebe7ec609a1452c
nwin19 <nandarwinbuma@gmail.com>
2023-12-15 11:17:25 +0630

    Part 18: Creaating movie suggestion

---
+++ src/components/MovieList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+
+const MovieList = ({ title, movieLists }) => {
+  console.log(movieLists);
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-3xl py-6 font-bold">{title}</h1>
+      <div className="flex overflow-x-scroll">
+        <div className="flex">
+          {movieLists?.map((movie) => (
+            <MovieCard key={movie.id} posterPath={movie?.poster_path} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
+  const movies = useSelector((store) => store.movies);
+  // console.log(movies);
   return (
-    <div>SecondaryContainer</div>
-  )
-}
+    movies.nowPlayingMovies && (
+      <div className="bg-black">
+        <div className="-mt-52  z-20 relative">
+          <MovieList
+            title={"Now Playing"}
+            movieLists={movies.nowPlayingMovies}
+          />
+          <MovieList title={"Trending"} movieLists={movies.nowPlayingMovies} />
+          <MovieList title={"Popular"} movieLists={movies.nowPlayingMovies} />
+          <MovieList
+            ti
+            tle={"Upcoming Movies"}
+            movieLists={movies.nowPlayingMovies}
+          />
+          <MovieList title={"Horror"} movieLists={movies.nowPlayingMovies} />
+        </div>
+      </div>
+    )
+  );
+};
 
-export default SecondaryContainer
+export default SecondaryContainer;
+
+/* 
+    MovieList - Popular
+      MovieCard * n 
+    MovieList - NowPlaying 
+    MovieList - Trending 
+    MovieList - Horror  
+  */
